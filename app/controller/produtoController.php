9710a48be31d960b5fe0cfa8d4b1c5cd4ff7b6a7
Matheus Raposo <123lampiao10@gmail.com>
2022-09-29 20:58:48 -0300

    Iniciado api REST

---
+++ app/controller/produtoController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\controller; // usando namespace para identificar a nossa classe
+
+class ProdutoController{
+    public function get(){
+
+    }
+
+    public function post(){
+
+    }
+
+    public function put($id){
+
+    }
+
+    public function delete($id){
+
+    }
+}
--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
-    //print_r($_GET);
-    $rota = !empty($_GET["url"]) ? $_GET["url"] : "Nada ainda";//Verifica se a url não está vazia, se estiver exibe um texto caso contrario atribui o conteudo normal.
-    echo $rota;
+require_once ("vendor/autoload.php");
+//use App\controller\ProdutoController;
+        //print_r($_GET);
+        $rota = !empty($_GET["url"]) ? $_GET["url"] : "Nada ainda";//Verifica se a url não está vazia, se estiver exibe um texto caso contrario atribui o conteudo normal.
+        echo $rota;
 
-    $rota = explode("/",$rota);// explode irá criar um vetor separando as informações quando encontrar uma barra(/).
-    echo "<hr>";
-    print_r($rota);
-    // Verificando se o usuário está querendo acessar a API
-    if($rota[0] === "api"){
-        echo "chegou na API <hr>" ;
-        array_shift($rota);
-
+        $rota = explode("/",$rota);// explode irá criar um vetor separando as informações quando encontrar uma barra(/).
+        echo "<hr>";
         print_r($rota);
-    }
-    else{
-        echo "Tá querendo acessar outra coisa né?";
-    }
+        // Verificando se o usuário está querendo acessar a API
+        if($rota[0] === "api"){
+            echo "chegou na API <hr>" ;
+            array_shift($rota);
+            print_r($rota);
+            //$meuProduto = new App\controller\ProdutoController();
+            //echo "<hr>";
+            //$meuProduto->inicio();
+
+            //pegando a informação do serviço que o usuário quer acessar 
+            if(!file_exists("App\controller\\".$rota[0]."controller.php")){
+                echo " <hr> esse serviço não está disponível";
+            }
+            else{
+                $servico = "App\Controller\\".ucfirst($rota[0])."Controller";//Organizando o caminho para deixar com o mesmo nome da classe da pasta controller
+                echo "<hr> {$servico}";
+
+                array_shift($rota);
+                $verboHttp = $_SERVER["REQUEST_METHOD"];// está pegando o verbo HTTP que é o método de envio dos dados, ou seja GET, POST, PUT, DELETE.
+
+            }
+
+        }
+        else{
+            echo "Tá querendo acessar outra coisa né?";
+        }
